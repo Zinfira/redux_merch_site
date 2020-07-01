2eec70d289e74752033e4ae10a288d42d280b2fe
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-01 10:27:19 -0700

    set up project dir. Make changes to App.js and work through components dir.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import React from "./Header";
+
+function App() {
+  const name = "Tony";
+  const name = "Chee";
+  return (
+    <React.Fragment>
+      <h1>Merch Site</h1>
+      <h3>Hoodies</h3>
+      <h3>{name} and {name2}</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+export default App;
