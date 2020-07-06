ba652de3a392116a4c1b58701ec83eeaffd53afd
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-06 15:28:04 -0700

    add a test for deleting an item and fail test

--- src/__tests__/item-list-reducer.test.js
+++ src/__tests__/item-list-reducer.test.js
@@ -10,6 +10,17 @@ describe('itemListReducer', () => {
     id: 1
   }
 
+  const currentState = {
+    1: {name: 'chocolate',
+    description: 'sweet desert',
+    quantity: 5,
+    id: 1 },
+    2: {name: 'yogurt',
+    description: 'milk produce',
+    quantity: 2,
+    id: 2 }
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(itemListReducer({}, { type: null })).toEqual({});
   });
@@ -31,6 +42,19 @@ describe('itemListReducer', () => {
         quantity: quantity,
         id: id
       }
+    });
+  });
+
+  test('Should successfully delete an item', () => {
+    action = {
+      type: 'DELETE_ITEM',
+      id: 1
+    };
+    expect(itemListReducer(currentState, action)).toEqual({
+      2: {name: 'yogurt',
+    description: 'milk produce',
+    quantity: 2,
+    id: 2 }
     });
   });
 });
